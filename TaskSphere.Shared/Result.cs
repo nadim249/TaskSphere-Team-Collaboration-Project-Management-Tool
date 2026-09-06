8fd4ea2c7eca4d8523f3acdaa78c172f1d5b9e48
nadim249 <mahmoudnadim47@gmail.com>
2026-09-07 01:06:59 +0600

    Shared Result of project

---
+++ TaskSphere.Shared/Result.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskSphere.Shared
+{
+    public class Result<T>
+    {
+        public T? Data { get; set; }
+        public bool HasError { get; set; }
+        public string Message { get; set; }
+    }
+}
