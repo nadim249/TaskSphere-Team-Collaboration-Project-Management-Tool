1b867505df87901e08dc3c920a2ed7eb53f6ba44
nadim249 <mahmoudnadim47@gmail.com>
2026-09-07 01:01:32 +0600

    dbcontest push

---
+++ TaskSphere.Data/TsDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using TaskSphere.Entities;
+
+namespace TaskSphere.Data
+{
+    public class TsDbContext(DbContextOptions<TsDbContext> options) : DbContext(options)
+    {
+       
+        public DbSet<PTask> Tasks { get; set; }
+        public  DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<TAttachment> Attachments { get; set; }
+        public DbSet<TaskStep> TaskSteps { get; set; }
+        public DbSet<Notification> Notifications { get; set; }  
+    }
+}
