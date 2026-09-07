c399422ffa83f8748135e044b6d2480af4e692db
nadim249 <mahmoudnadim47@gmail.com>
2026-09-07 13:47:24 +0600

    current user shared

---
+++ TaskSphere.Shared/CurrentUserHelper.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TaskSphere.Shared;
+
+public class CurrentUserHelper(IHttpContextAccessor accessor)
+{
+    public bool IsAuthenticated
+    {
+        get
+        {
+            try
+            {
+                return (bool)accessor.HttpContext?.User?.Identity?.IsAuthenticated;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+        }
+    }
+
+    public int UserId
+    {
+        get
+        {
+            try
+            {
+                var id = accessor.HttpContext?.User?
+                    .FindFirst("UserId").Value;
+                return id!=null? int.Parse(id) : -1;
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+
+        }
+    }
+
+    public string Email
+    {
+        get
+        {
+            try
+            {
+                var email = accessor.HttpContext?.User?
+                    .FindFirst("Email").Value;
+                return email != null ? email : "-";
+            }
+            catch (Exception e)
+            {
+                return "-";
+            }
+
+        }
+    }
+}
