dd2975ebe84a9cf65f491dda4a6a3bb1d8b3a19c
nadim249 <mahmoudnadim47@gmail.com>
2026-09-07 13:52:40 +0600

    auth, team task and team project views

---
+++ TaskSphere.Web/Models/EmailSettings.cs
@@ -0,0 +1,10 @@
+namespace TaskSphere.Web.Models
+{
+    public class EmailSettings
+    {
+        public string SmtpServer { get; set; } = string.Empty;
+        public int SmtpPort { get; set; }
+        public string SenderEmail { get; set; } = string.Empty;
+        public string SenderPassword { get; set; } = string.Empty;
+    }
+}
